8e99e0181b95c6b7ec2b58beb703dccb5dec869a
Zane Helton <zane99@me.com>
2026-01-14 00:38:02 -0500

    Add .TrueColor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -31,7 +31,7 @@ func main() {
 		wish.WithAddress(fmt.Sprintf("%s:%s", host, sshPort)),
 		wish.WithHostKeyPath(".ssh/id_ed25519"),
 		wish.WithMiddleware(
-			bubbletea.MiddlewareWithProgramHandler(tui.NewProgramHandler(grpcAddr), termenv.Ascii),
+			bubbletea.MiddlewareWithProgramHandler(tui.NewProgramHandler(grpcAddr), termenv.TrueColor),
 			logging.Middleware(),
 		),
 	)
